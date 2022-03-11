5582cc387fae99286f56440514fedf251cfc69b4
Evan Mei <em@Evans-MacBook-Pro.local>
2022-03-11 11:18:45 -0500

    finished side bar event listener and css for center spotlight

--- script.js
+++ script.js
@@ -1,19 +1,78 @@
-for (let i = 0; i < 10; i++) {
-  let numGrabs = i + 1;
-  console.log(numGrabs)
-  fetch("https://pokeapi.co/api/v2/pokemon/" + numGrabs)
-    .then(res => res.json())
-    .then(result => {
-      console.log(result)
-      let sideBarPokes = document.getElementsByClassName("spriteimgs")
-      sideBarPokes[i].src = result.sprites.front_default;
-      let sidePokename = document.getElementsByClassName("pokeList");
-      sidePokename[i].innerText = result.name
+let sideBarPokes = document.querySelectorAll(".spriteimgs");
+let sidePokename = document.querySelectorAll(".pokeList");
+let spotlightPokemon = document.querySelector(".pokePhoto")
+let spriteContainer = document.querySelector(".pokemons")
+
+
+// sidebarIdTracker = [];
+// console.log(sidebarIdTracker)
+
+// for (let i = 0; i < 10; i++) {
+//   let numGrabs = i + 1;
+//   console.log(numGrabs)
+//   fetch("https://pokeapi.co/api/v2/pokemon/" + numGrabs)
+//     .then(res => res.json())
+//     .then(result => {
+//       console.log(result)
+//       sideBarPokes[i].src = result.sprites.front_default;
+//       sidePokename[i].innerText = result.name;
+//       sidebarIdTracker.push(result.id);
+
+//     })
+// } 
+
+function getPokemon(n) {
+  for (let i = 1; i <= n; i++) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      .then(res => res.json())
+      .then(result => {
+        renderSprites(result)
+      })
+  } 
+}
+getPokemon(9)
+
+function renderSprites(data) {
+  let sprite = `
+    <li>
+      <a class="pokeList">${data.name}</a>
+      <img src="${data.sprites.front_default}" class="spriteimgs" name="${data.id}">
+    </li>
+  `
+  spriteContainer.insertAdjacentHTML("beforeend", sprite)
+
+  let sprites = document.querySelectorAll(".spriteimgs")
+
+  sprites.forEach((sprt) => {
+    sprt.addEventListener("click", () => {
+      if (data.id == sprt.name) {
+        spotlightPokemon.src = data.sprites.front_default
+      }
     })
-} 
+  })
+}
 
 
-  
+// sidePokename[i].addEventListener("click", function () {
+//     let numGrabs2 = i;
+//     fetch("https://pokeapi.co/api/v2/pokemon/" + numGrabs)
+//       .then(res => res.json())
+//       .then(result => { 
+//       spotlightPokemon.src = result.sprites.other.dream_world.front_default
+//       })
+//    })
+
+
+
+// for (let i = 0; 1 < sidebarIdTracker.length; i++) {
+//   sideBarPokes[i].addEventListener("click", enlarge(i))
+// }
+
+// function enlarge(i) {
+//   return function () {
+//     console.log("you clicked #" + i)
+//   }
+// }
   
 function toggleSidebar() {
   document.getElementById("sidebar").classList.toggle('unhide');
