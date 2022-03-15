b965e568c52c389f754c3f13abf7784afecaddbf
Evan Mei <em@Evans-MacBook-Pro.local>
2022-03-15 18:32:00 -0400

    added player 2 and battle condition icon with event listener

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 let sideBarPokes = document.querySelectorAll(".spriteimgs");
 let sidePokename = document.querySelectorAll(".pokeList");
-let spotlightPokemon = document.querySelector(".pokePhoto");
-let spriteContainer = document.querySelector(".pokemons");
+let spotlightPokemonOne = document.querySelector(".pokePhotoOne");
+let spotlightPokemonTwo = document.querySelector(".pokePhotoTwo");
+// let spriteContainer = document.querySelectorAll(".pokemons");
+let spriteContainerOne = document.querySelectorAll(".pokemonsOne");
+let spriteContainerTwo = document.querySelectorAll(".pokemonsTwo");
 let genoneButton = document.getElementById("genone");
 let gentwoButton = document.getElementById("gentwo");
 let genthreeButton = document.getElementById("genthree");
 let randoButton = document.getElementById("random");
 let leftInfo = document.querySelector(".leftbox");
-let rightInfo = document.querySelector(".rightbox");
-let leftPhoto = document.querySelector(".leftimage");
-let rightPhoto = document.querySelector(".rightimage");
+let pokeInfo = document.querySelector(".bottomrightbox");
+let leftPhoto = document.querySelector(".leftImage");
+let rightPhoto = document.querySelector(".rightImage");
+let battleIcon = document.querySelector("#battleCenter")
+let playerOneBattleCondition = 0;
+let playerTwoBattleCondition = 0;
+console.log(playerOneBattleCondition, playerTwoBattleCondition)
+console.log(battleIcon)
 
-console.log(leftInfo.innerText)
+
 // sidebarIdTracker = [];
 // console.log(sidebarIdTracker)
 
@@ -29,13 +37,72 @@ console.log(leftInfo.innerText)
 //     })
 // } 
 
+// --------------------------Working Code------------------------
+
+// function getPokemon(n) {
+//   for (let i = n; i <= n + 9 ; i++) {
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+//       .then(res => res.json())
+//       .then(result => {
+//         // console.log(result)
+//         renderSprites(result)
+//       })
+//   } 
+// }
+
+// getPokemon(1)
+
+
+// function renderSprites(data) {
+//   let sprite =
+//     `
+//     <li>
+//       <a class="pokeList">${data.name}</a>
+//       <img src="${data.sprites.front_default}" class="spriteimgs" name="${data.id}">
+//     </li>
+//   `
+//   for (let i = 0; i < spriteContainer.length; i++) {
+//     spriteContainer[i].insertAdjacentHTML("beforeend", sprite)
+//   }
+//   let sprites = document.querySelectorAll(".spriteimgs")
+
+//   sprites.forEach((sprt) => {
+//     sprt.addEventListener("click", () => {
+//       if (data.id == sprt.name) {
+//         console.log(data.id)
+//         console.log(sprt.name)
+//         console.log(data)
+//         spotlightPokemon.src = data.sprites.front_default
+//         leftPhoto.src = data.sprites.other["official-artwork"].front_default
+//         // pokeName.insertAdjacentHTML("beforeend", "Name: " + data.forms["0"].name)
+//         function chgback() {
+//           if (data.types[0].type.name == "grass") {
+//             document.body.style.backgroundColor = "#20b49c";
+//           }
+//           else if (data.types[0].type.name == "fire") {
+//             document.body.style.backgroundColor = "#ee8328";
+//           }
+//           else if (data.types[0].type.name == "water") {
+//             document.body.style.backgroundColor = "#84ADD7";
+//           }
+//         }
+//         chgback();
+//       }
+     
+//     })
+//   })
+// }
+
+// --------------------------Working Code------------------------
+
 function getPokemon(n) {
   for (let i = n; i <= n + 9 ; i++) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
       .then(res => res.json())
       .then(result => {
         // console.log(result)
-        renderSprites(result)
+        renderSpritesOne(result)
+        renderSpritesTwo(result)
       })
   } 
 }
@@ -43,75 +110,118 @@ function getPokemon(n) {
 getPokemon(1)
 
 
-function renderSprites(data) {
-  let sprite = 
-  `
+function renderSpritesOne(data) {
+  let sprite =
+    `
     <li>
       <a class="pokeList">${data.name}</a>
-      <img src="${data.sprites.front_default}" class="spriteimgs" name="${data.id}">
+      <img src="${data.sprites.front_default}" class="spriteimgsOne" name="${data.id}">
     </li>
   `
-  spriteContainer.insertAdjacentHTML("beforeend", sprite)
+  for (let i = 0; i < spriteContainerOne.length; i++) {
+    spriteContainerOne[i].insertAdjacentHTML("beforeend", sprite)
+  }
+  let sprites = document.querySelectorAll(".spriteimgsOne")
 
-  let sprites = document.querySelectorAll(".spriteimgs")
-
-  sprites.forEach((sprt) => {
-    sprt.addEventListener("click", () => {
-      if (data.id == sprt.name) {
+  sprites.forEach((e) => {
+    e.addEventListener("click", () => {
+      if (data.id == e.name) {
         console.log(data.id)
-        console.log(sprt.name)
+        console.log(e.name)
         console.log(data)
-        spotlightPokemon.src = data.sprites.front_default
+        spotlightPokemonOne.src = data.sprites.front_default
         leftPhoto.src = data.sprites.other["official-artwork"].front_default
-        rightPhoto.src = data.sprites.other.home.front_default
-        function chgback() {
-          if (data.types[0].type.name == "grass") {
-            document.body.style.backgroundColor = "#20b49c";
-          }
-          else if (data.types[0].type.name == "fire") {
-            document.body.style.backgroundColor = "#ee8328";
-          }
-          else if (data.types[0].type.name == "water" ){
-            document.body.style.backgroundColor = "#84ADD7";
-          }
-        }
-        chgback();
+        // pokeName.insertAdjacentHTML("beforeend", "Name: " + data.forms["0"].name)
+        // function chgback() {
+        //   if (data.types[0].type.name == "grass") {
+        //     document.body.style.backgroundColor = "#20b49c";
+        //   }
+        //   else if (data.types[0].type.name == "fire") {
+        //     document.body.style.backgroundColor = "#ee8328";
+        //   }
+        //   else if (data.types[0].type.name == "water") {
+        //     document.body.style.backgroundColor = "#84ADD7";
+        //   }
+        // }
+        // chgback();
+       
+        battleCheckOne()
+        battle() 
       }
      
     })
   })
 }
 
+function renderSpritesTwo(data) {
+  let sprite =
+    `
+    <li>
+      <a class="pokeList">${data.name}</a>
+      <img src="${data.sprites.front_default}" class="spriteimgsTwo" name="${data.id}">
+    </li>
+  `
+  for (let i = 0; i < spriteContainerTwo.length; i++) {
+    spriteContainerTwo[i].insertAdjacentHTML("beforeend", sprite)
+  }
+  let sprites = document.querySelectorAll(".spriteimgsTwo")
+
+  sprites.forEach((e) => {
+    e.addEventListener("click", () => {
+      if (data.id == e.name) {
+        console.log(data.id)
+        console.log(e.name)
+        console.log(data)
+        spotlightPokemonTwo.src = data.sprites.front_default
+        rightPhoto.src = data.sprites.other["official-artwork"].front_default
+        // pokeName.insertAdjacentHTML("beforeend", "Name: " + data.forms["0"].name)
+        // function chgback() {
+        //   if (data.types[0].type.name == "grass") {
+        //     document.body.style.backgroundColor = "#20b49c";
+        //   }
+        //   else if (data.types[0].type.name == "fire") {
+        //     document.body.style.backgroundColor = "#ee8328";
+        //   }
+        //   else if (data.types[0].type.name == "water") {
+        //     document.body.style.backgroundColor = "#84ADD7";
+        //   }
+        // }
+        // chgback();
+        battleCheckTwo()
+        battle() 
+      }
+     
+    })
+  })
+}
 
 genoneButton.addEventListener("click", function () {
   console.log("heres gen 1!")
-  reset()
+  resetOne()
+  resetTwo()
   getPokemon(1);
- 
 }) 
 
 gentwoButton.addEventListener("click", function () {
   console.log("heres gen 2!")
-  reset()
+  resetOne()
+  resetTwo()
   getPokemon(152);
- 
 }) 
 
 genthreeButton.addEventListener("click", function () {
   console.log("heres gen 3!")
-  reset()
+  resetOne()
+  resetTwo()
   getPokemon(252);
-  
-
-  
 }) 
 
 
 
 randoButton.addEventListener("click", function () {
-    console.log("heres random!")
-  reset()
-  console.log(randomNumber(1, 881));
+  console.log("heres random!")
+  resetOne()
+  resetTwo()
   getPokemon(randomNumber(1, 881));
 
 
@@ -121,10 +231,52 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max-min) + min);
 }
 
-function reset() {
-  document.querySelector('.pokemons').innerHTML= '';
-} 
+function resetOne() {
+  let elements = document.querySelectorAll('.pokemonsOne')
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].innerHTML = ''
+  }
+}
+
+function resetTwo() {
+    let elements = document.querySelectorAll('.pokemonsTwo')
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].innerHTML = ''
+    }
+  }
+
+
+  function battleCheckOne() {
+    if (leftPhoto.src != " ") {
+      playerOneBattleCondition = 1;
+    }
+  }
+
+
+  function battleCheckTwo() {
+    if (rightPhoto.src != " ") {
+      playerTwoBattleCondition = 1;
+    }
+  }
+
+function battle() {
+  if ((playerOneBattleCondition = 1) && (playerTwoBattleCondition = 1)) {
+    console.log(playerOneBattleCondition, playerTwoBattleCondition)
+    battleIcon.src = "battle.png";
+    }
+  }
+
   
+
+  // let spriteContainer = document.querySelectorAll(".pokemons");
+  // console.log(spriteContainer[0])
+  // for (let i = 0; i > 3; i++) {
+  //   spriteContainer[i].innerHTML = ' ';
+  // }
+  //console.log(document.querySelector(".pokemons"))
+  //  document.querySelector(".pokemons").innerHTML = " ";
+
+
 
 // sidePokename[i].addEventListener("click", function () {
 //     let numGrabs2 = i;
@@ -149,4 +301,5 @@ function reset() {
   
 function toggleSidebar() {
   document.getElementById("sidebar").classList.toggle('unhide');
+  document.getElementById("sidebar2").classList.toggle('unhide');
 }
