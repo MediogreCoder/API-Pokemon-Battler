4153518d01588cf215954956dc917481de7221c2
Evan Mei <em@Evans-MacBook-Pro.local>
2022-03-20 17:58:29 -0400

    added comments in js and reverted dead aniamtion for p2

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
-let sideBarPokes = document.querySelectorAll(".spriteimgs");
-let bigPokeBall = document.getElementById("pokeall");
-let sidePokename = document.querySelectorAll(".pokeList");
-let spotlightPokemonOne = document.querySelector(".p1p");
-let spotlightPokemonTwo = document.querySelector(".p2p");
-let spriteContainerOne = document.querySelectorAll(".pokemonsOne");
-let spriteContainerTwo = document.querySelectorAll(".pokemonsTwo");
-let genoneButton = document.getElementById("genone");
-let gentwoButton = document.getElementById("gentwo");
-let genthreeButton = document.getElementById("genthree");
-let randoButton = document.getElementById("random");
-let leftInfo = document.querySelector(".leftbox");
-let pokeInfo = document.querySelector(".bottomrightbox");
-let leftPhoto = document.querySelector(".leftImage");
-let rightPhoto = document.querySelector(".rightImage");
-let battleIcon = document.querySelector(".centerIcon");
-let p1PokeMoves = document.querySelectorAll(".p1");
-let p2PokeMoves = document.querySelectorAll(".p2");
-let playerOneHealth = document.getElementById("p1Health");
-let playerTwoHealth = document.getElementById("p2Health");
-let playerOneBattleCondition = 0;
-let playerTwoBattleCondition = 0;
-let deadCheckOne = 0;
-let deadCheckTwo = 0;
-let chosenNameOne = "";
-let chosenNameTwo = "";
-console.log(p1PokeMoves)
+let sideBarPokes = document.querySelectorAll(".spriteimgs"); // burger menu sprites
+let bigPokeBall = document.getElementById("pokeall"); //center piece
+let sidePokename = document.querySelectorAll(".pokeList"); //burger menu names
+let spotlightPokemonOne = document.querySelector(".p1p"); //chosen sprite in center
+let spotlightPokemonTwo = document.querySelector(".p2p"); //chosen sprite in center
+let spriteContainerOne = document.querySelectorAll(".pokemonsOne"); //container for sprites in burger
+let spriteContainerTwo = document.querySelectorAll(".pokemonsTwo"); //container for sprites in burger
+let genoneButton = document.getElementById("genone"); //buttons to cycle through gens 1
+let gentwoButton = document.getElementById("gentwo"); //buttons to cycle through gens 2
+let genthreeButton = document.getElementById("genthree"); //buttons to cycle through gens 3
+let randoButton = document.getElementById("random"); //buttons to random
+let leftInfo = document.querySelector(".leftbox"); // entire left colunm
+let pokeInfo = document.querySelector(".bottomrightbox"); //bottom right box for art and attacks
+let leftPhoto = document.querySelector(".leftImage"); //left art
+let rightPhoto = document.querySelector(".rightImage"); //right art
+let battleIcon = document.querySelector(".centerIcon"); //center icon, battle visual
+let p1PokeMoves = document.querySelectorAll(".p1"); //player one attacks
+let p2PokeMoves = document.querySelectorAll(".p2"); //player two attacks
+let playerOneHealth = document.getElementById("p1Health"); //player one health
+let playerTwoHealth = document.getElementById("p2Health"); //player two health
+let playerOneBattleCondition = 0; //check if p1 picked
+let playerTwoBattleCondition = 0; //check if p2 picked
+let deadCheckOne = 0; //check if p1 pokemon has 0 hp
+let deadCheckTwo = 0; //check if p2 pokemon has 0 hp
+let chosenNameOne = ""; //used to fill pokenam after pick
+let chosenNameTwo = ""; //used to fill pokenam after pick
 
+
+//API Glossary:
 //Pokemon Health = data.stats[0].base_stat
+//Pokemon Name = data.name
+//Pokemon Front View Sprit = data.sprites.front_default
+//Official artwork = data.sprites.other["official-artwork"].front_default
+//attack move link from api = data.moves.move.url
 
-
+// main fetch to populate burger menu
 function getPokemon(n) {
   for (let i = n; i <= n + 8 ; i++) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
       .then(res => res.json())
       .then(result => {
-        // //console.log(result)
         renderSpritesOne(result)
         renderSpritesTwo(result)
       })
   } 
 }
 
+//grabs the default 9 first gen starters
 getPokemon(1)
 
-
+//runs after fetch to populate the burger menu- generates list objects and inserts the HTL directly into container
 function renderSpritesOne(data) {
   let sprite =
     `
@@ -56,38 +61,37 @@ function renderSpritesOne(data) {
     spriteContainerOne[i].insertAdjacentHTML("beforeend", sprite)
   }
   let sprites = document.querySelectorAll(".spriteimgsOne")
-
+//event listeners for pokemon selection 
   sprites.forEach((e) => {
     e.addEventListener("click", () => {
       if (data.id == e.name) {
         console.log(data.name)
         chosenNameOne = data.name;
         spotlightPokemonOne.src = data.sprites.front_default;
-
         spotlightPokemonOne.style.display = "flex"
         leftPhoto.src = data.sprites.other["official-artwork"].front_default;
         playerOneHealth.value = (10 * (data.stats[0].base_stat));
         playerOneHealth.max = (10 * (data.stats[0].base_stat));
+//fills the 4 move spaces with attack name and power from attack URL api
         {
           for (let i = 0; i < 4; i++) {
-            // console.log(data.stats)
-            // console.log(p1PokeMoves[i].innerText)
             p1PokeMoves[i].innerText = data.moves[i].move.name;
               fetch(data.moves[i].move.url)
               .then(res => res.json())
               .then(result => {
-                //console.log(result.power)
+                console.log(data.moves[i])
                 {p1PokeMoves[i].dataset.num = result.power }
                })
           }
         }
+//after every selecting, marks down whether we can battle or not and spawns the battle icon
         battleCheckOne()
         battle() 
       }
     })
   })
 }
-
+// functionally the same as the code block for player one
 function renderSpritesTwo(data) {
   let sprite =
     `
@@ -104,9 +108,6 @@ function renderSpritesTwo(data) {
   sprites.forEach((e) => {
     e.addEventListener("click", () => {
       if (data.id == e.name) {
-        //console.log(data.id)
-        //console.log(e.name)
-        //console.log(data)
         chosenNameTwo = data.name;
         spotlightPokemonTwo.src = data.sprites.front_default
         spotlightPokemonTwo.style.display = "flex"
@@ -116,19 +117,14 @@ function renderSpritesTwo(data) {
       
         {
           for (let i = 0; i < 4; i++) {
-            //console.log(data.moves[i].move.name)
-            //console.log(p2PokeMoves[i].innerText)
             p2PokeMoves[i].innerText = data.moves[i].move.name;
-            //console.log("here",p2PokeMoves[i].dataset)
             fetch(data.moves[i].move.url)
               .then(res => res.json())
               .then(result => {
-                //console.log(result.power)
                 {p2PokeMoves[i].dataset.num = result.power }
                })
           }
-        }
-               
+        }     
         battleCheckTwo()
         battle() 
       }
@@ -137,29 +133,28 @@ function renderSpritesTwo(data) {
   })
 }
 
+//event listeners for the 4 buttons to change burger menu pokemin between generations, hard coded for the starters
+//runs original fetch after resetting(deletes current selection)
+
 genoneButton.addEventListener("click", function () {
-  //console.log("heres gen 1!")
   resetOne()
   resetTwo()
   getPokemon(1);
 }) 
 
 gentwoButton.addEventListener("click", function () {
-  //console.log("heres gen 2!")
   resetOne()
   resetTwo()
   getPokemon(152);
 }) 
 
 genthreeButton.addEventListener("click", function () {
-  //console.log("heres gen 3!")
   resetOne()
   resetTwo()
   getPokemon(252);
 }) 
 
 randoButton.addEventListener("click", function () {
-  //console.log("heres random!")
   resetOne()
   resetTwo()
   getPokemon(randomNumber(1, 881));
@@ -167,10 +162,12 @@ randoButton.addEventListener("click", function () {
 
 })
 
+//random number generator for random buttom, can be purposed for future random needs
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max-min) + min);
 }
 
+//menu and board clear functions
 function resetOne() {
   let elements = document.querySelectorAll('.pokemonsOne')
   for (let i = 0; i < elements.length; i++) {
@@ -185,13 +182,12 @@ function resetTwo() {
     }
   }
 
-
+//checks if we can go in battle, req both players have to pick
   function battleCheckOne() {
     if (leftPhoto.src != " ") {
       playerOneBattleCondition = 1;
     }
   }
-
 
   function battleCheckTwo() {
     if (rightPhoto.src != " ") {
@@ -199,16 +195,16 @@ function resetTwo() {
     }
   }
 
+//activates battle icon
 function battle() {
   if ((playerOneBattleCondition == 1) && (playerTwoBattleCondition == 1)) {
-    //console.log(playerOneBattleCondition, playerTwoBattleCondition)
     battleIcon.src = "battle.png";
     battleIcon.classList.add("battleCenter")
     }
-  }
-
+}
+  
+//main function to intiate battle page, populates attacks and removes pokeball, toggles sidebar, has a 2 second delay
 battleIcon.addEventListener("click", gottaGoFast) 
-
 
   function gottaGoFast() {
     document.getElementById("backdrop").style.animation = "rotation 2s infinite linear";
@@ -240,52 +236,8 @@ battleIcon.addEventListener("click", gottaGoFast)
     }
   }
 
-// working attack code-------------------------------------------------------------------
-// p1PokeMoves.forEach(pickAttack1 => {
-//   pickAttack1.addEventListener("click", p1whichAttack) 
-// })
 
-// function p1whichAttack() {
-//   for (let i = 0; i < 4; i++) {
-//     if (i == p1PokeMoves[i]) {
-//       playerTwoHealth.value -= parseInt(p1PokeMoves[i].dataset.num)
-//       console.log(playerTwoHealth.value)
-//     }
-//   }
-// }
-
-// p2PokeMoves.forEach(pickAttack2 => {
-  
-//   pickAttack2.addEventListener("click", p2whichAttack) 
-// })
-
-// function p2whichAttack() {
-//   for (let i = 0; i < 4; i++) {
-//     // console.log(p2PokeMoves[i].dataset.num)
-//     playerOneHealth.value -= parseInt(p2PokeMoves[i].dataset.num)
-    
-//       console.log(playerOneHealth.value)
-//       deadCheckoOne = playerOneHealth.value
-//       p1CheckDead()
-    
-//   }
-// }
-
-// function p2CheckDead() {
-//   if (deadCheckoTwo == 0) {
-//     console.log(deadCheckTwo)
-//     console.log("he dead")
-//   }
-    
-// }
-
-// function p2CheckDead() {
-//   if (deadCheckTwo == 0) {
-//     console.log("he dead")
-//   }
-// }
-// working attack code-------------------------------------------------------------------
-
+//damage the oposite HP upon clicking an attack. Will subtract attack dmg from HP dataset, has check function to end game
 for(var i = 0; i < p1PokeMoves.length; i++) {
   p1PokeMoves[i].addEventListener("click", doDamagetoP2(i));
 }
@@ -298,19 +250,6 @@ function doDamagetoP2(i) {
 };
 }
 
-// for(var i = 0; i < p2PokeMoves.length; i++) {
-//   p2PokeMoves[i].addEventListener("click", doDamagetoP1(i));
-// }
-
-// function doDamagetoP1(i) {
-//   return function () {
-//     playerOneHealth.value -= parseInt(p2PokeMoves[i].dataset.num);
-//     deadCheckoOne = playerOneHealth.value;
-//     console.log(playerOneHealth.value);
-//     p1CheckDead();
-    
-// };
-// }
 
 for (let i = 0; i < p2PokeMoves.length; i++) {
   p2PokeMoves[i].addEventListener("click", doDamagetoP1(i));
@@ -324,6 +263,7 @@ function doDamagetoP1(i) {
   }
 }
 
+//checks if HP is 0, flips dead aniamtions and shows new game button
 function p2CheckDead() {
   if (deadCheckTwo == 0) {
     spotlightPokemonTwo.classList.add("p2Dead")
@@ -340,6 +280,7 @@ function p1CheckDead() {
   }
 }
 
+//reveals and hides sidebar
 function toggleSidebar() {
   document.getElementById("sidebar").classList.toggle('unhide');
   document.getElementById("sidebar2").classList.toggle('unhide');
